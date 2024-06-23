fc2dfcf7537b920cdd6aa0aaecb68ef7a5df69eb
aeldab3 <aeldabaa2@gmail.com>
2024-06-23 21:35:48 +0300

    Add project files.

---
+++ API/API/Controllers/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private StoreContext _Context;
+        
+        public ProductsController(StoreContext Context)
+        {
+            _Context = Context;
+        }
+            [HttpGet]
+            public async Task <ActionResult<List<Product>>> GetProducts()
+            {
+                var products = await _Context.Products.ToListAsync();
+                return Ok(products);
+            }
+
+            [HttpGet("{id}")]
+            public async Task<ActionResult<Product>> GetProduct(int id)
+            {
+                var product = await _Context.Products.SingleOrDefaultAsync(x => x.Id == id);
+                return Ok(product);
+            }
+      
+    }
+}
---
+++ API/Core/Entities/Product.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [MaxLength(100)]
+        public string Name { get; set; }
+    }
+}
