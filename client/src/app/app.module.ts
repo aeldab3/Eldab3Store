a7b46a5400bba20d152da989492da6f466f5b7cf
aeldab3 <aeldabaa2@gmail.com>
2024-07-05 05:37:18 +0300

    Save current work before updating

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
