881a2a1e3a23ad5a449c6b325523b1a0458834fd
aeldab3 <aeldabaa2@gmail.com>
2024-07-05 06:12:33 +0300

    commit for update

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
